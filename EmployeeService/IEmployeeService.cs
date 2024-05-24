d29562c30678c04479492d2dee87a4af3c822e53
KapustaDlyaMangusta <maksick2005@gmail.com>
2024-05-24 15:24:48 +0300

    Add project files.

---
+++ EmployeeService/IEmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "GetEmployeeById?id={id}",
+            ResponseFormat = WebMessageFormat.Json,  BodyStyle = WebMessageBodyStyle.Bare)]
+        bool GetEmployeeById(int id);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "EnableEmployee?id={id}", 
+            BodyStyle = WebMessageBodyStyle.WrappedRequest)]
+        void EnableEmployee(int id, int enable);
+    }
+
+	
+}
---
+++ InterviewConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dtEmployees = GetQueryResult("SELECT * FROM Employee");
+        }
+        
+        private static DataTable GetQueryResult(string query)
+        {
+            var dt = new DataTable();
+
+			using (var connection = new SqlConnection("Data Source=(local);Initial Catalog=Test;User ID=sa;Password=pass@word1; "))
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+					command.CommandText = query;
+
+                    using (var adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.Fill(dt);
+                    }
+                }
+            }
+
+			return dt;
+        }
+    }
+}
